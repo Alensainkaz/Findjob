36dafb133782dd4fe981429cff849c34102b276c
Alensainkaz <alen232kz@gmail.com>
2026-03-20 11:21:10 +0500

    Fixing mistakes

--- Jobfinder/settings.py
+++ Jobfinder/settings.py
@@ -92,8 +92,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL=True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
